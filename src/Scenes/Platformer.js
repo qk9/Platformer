73efcc669be928fa4a4d8dc64793a5fdb90d7389
qk9 <iain6475@gmail.com>
2026-05-12 00:33:16 -0700

    unfinished velocity storage mechanic (need to move velo storage onto its own button and make jump comboable into dash)

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -45,6 +45,7 @@ class Platformer extends Phaser.Scene {
         this.controls.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.controls.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.controls.jump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.controls.dash = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
 
         this.player = new Player(this, 250, game.config.height - 200, this.controls, 3, "playerTexture", null);
 
--- src/Sprites/Player.js
+++ src/Sprites/Player.js
@@ -4,11 +4,13 @@ class Player extends Phaser.GameObjects.Sprite {
                 numTriangles, texture, frame) {
         super(scene, x, y, texture, frame);
 
+        // initialize physics body
         this.body = new Phaser.Physics.Arcade.Body(scene.physics.world, this);
         let collisionRadius = 25;
         this.body.setSize(collisionRadius * 2, collisionRadius * 2, this.displayWidth / 2 - collisionRadius, this.displayHeight / 2 - collisionRadius);
         this.body.setBoundsRectangle(new Phaser.Geom.Rectangle(0, 0, scene.map.widthInPixels, scene.map.heightInPixels));
-        
+
+        // create rotating triangles
         this.numTriangles = numTriangles;
         this.triangleScale = 200;
         this.triangleGraphics = this.scene.add.graphics();
@@ -22,25 +24,49 @@ class Player extends Phaser.GameObjects.Sprite {
             this.triangleGraphics.strokeTriangleShape(this.triangles[i]);
         }
 
+        // initialize player controls
         this.controls = controls;
-        this.startingJumpVelo = -460;
-        this.jumpVelo = this.startingJumpVelo;
-        this.maxJumpVelo = 1500;
+
+        // player movement constants
+        this.maxFallVelo = 900; // prevent player from glitching through walls (thanks phaser)
+        this.dragMultiplier = 1;
+
+        // momentum storage variables
+        this.storedVelo = 0;
+        this.maxStoredVelo = 1500;
         this.lastXVelo = this.body.maxVelocity.x + 1;
+        this.canStoreVelo = true;
 
+        // jump callback & variables
+        this.minJumpVelo = -460;
         this.controls.jump.on("up", () => {
-            if (this.body.onFloor()) {
-                console.log("jump at velo", this.jumpVelo * -1);
-                this.body.setVelocityY(this.jumpVelo);
+            if (this.body.onFloor() && this.canStoreVelo) {
+                console.log("jump at velo", this.storedVelo);
+                this.body.setVelocityY((this.storedVelo * -1) + this.minJumpVelo);
             }
-            this.jumpVelo = this.startingJumpVelo;
+            this.storedVelo = 0;
+            this.canStoreVelo = true;
 
-            // set startvelo to inactive by making it > player's max speed
+            // flag lastXVelo as invalid by making it > player's max speed
             this.lastXVelo = this.body.maxVelocity.x + 1;
         });
 
+        // dash callback & variables
+        this.dashVelo = 0;
+        this.moveDirection = 0; // -1 = left, 0 = neutral, 1 = right
+        this.controls.dash.on("down", () => {
+            this.body.setVelocityX(this.body.velocity.x + this.storedVelo * this.moveDirection);
+
+            if (this.storedVelo > 0) {
+                this.canStoreVelo = false;
+            }
+            // remove spent dashVelo from storedVelo; use += because storedVelo is negative
+            this.storedVelo = 0;
+        })
+
         this.acceleration = 500;
         this.body.setMaxVelocityX(1000);
+        this.body.setMaxVelocityY(1425);
         this.drag = 700;
         this.scene.children.bringToTop(this);
 
@@ -63,40 +89,105 @@ class Player extends Phaser.GameObjects.Sprite {
             this.triangleGraphics.strokeTriangleShape(this.triangles[i]);
         }
 
-        if (this.controls.left.isDown && !this.controls.right.isDown) {
-            this.body.setAccelerationX(this.acceleration * -1);
-            this.body.setDragX(0);
-            this.rotation += (Math.PI / -6 - this.rotation) * 0.01;
-        }
-        else if (this.controls.right.isDown && !this.controls.left.isDown) {
-            this.body.setAccelerationX(this.acceleration);
-            this.body.setDragX(0);
-            this.rotation += (Math.PI / 6 - this.rotation) * 0.01;
-        }
-        else if (this.controls.left.isDown && this.controls.right.isDown) {
-            this.body.setAccelerationX(0);
-            this.body.setDragX(this.drag * 3);
-            this.rotation -= this.rotation * 0.03;
-        }
-        else {
-            this.body.setAccelerationX(0);
-            this.body.setDragX(this.drag);
-            this.rotation -= this.rotation * 0.01;
+        /*// left/right movement
+        if (this.body.onFloor()) {
+            if (this.controls.left.isDown && !this.controls.right.isDown) {
+                this.body.setAccelerationX(this.acceleration * -1);
+                this.body.setDragX(0);
+                this.rotation += (Math.PI / -6 - this.rotation) * 0.01;
+            }
+            else if (this.controls.right.isDown && !this.controls.left.isDown) {
+                this.body.setAccelerationX(this.acceleration);
+                this.body.setDragX(0);
+                this.rotation += (Math.PI / 6 - this.rotation) * 0.01;
+            }
+            else if (this.controls.left.isDown && this.controls.right.isDown) {
+                this.body.setAccelerationX(0);
+                this.body.setDragX(this.drag);
+                this.rotation -= this.rotation * 0.03;
+            }
+            else {
+                this.body.setAccelerationX(0);
+                this.body.setDragX(this.drag * 3);
+                this.rotation -= this.rotation * 0.01;
+            }
         }
 
         if (this.controls.jump.isDown && this.body.onFloor()) {
-            if (Math.abs(this.lastXVelo) < this.body.maxVelocity.x && Math.abs(this.jumpVelo) < this.maxJumpVelo) {
-                console.log(Math.abs(this.lastXVelo - this.body.velocity.x) * -1);
-                this.jumpVelo += Math.max(Math.abs(this.lastXVelo - this.body.velocity.x) * -1, -87.5);
-            }
-            this.lastXVelo = this.body.velocity.x;
+            // reset dash velo
+            this.dashVelo = 0;
+
+            // kill acceleration and update drag if needed
             this.body.setAccelerationX(0);
             if (this.body.drag.x < this.drag) {
                 this.body.setDragX(this.drag);
             }
 
-            if (Math.abs(this.jumpVelo) > this.maxJumpVelo) {
-                this.jumpVelo = this.maxJumpVelo * -1;
+            // add siphoned xVelo to jump height
+            if (Math.abs(this.lastXVelo) < this.body.maxVelocity.x && Math.abs(this.storedVelo) < this.maxStoredVelo) {
+                console.log(Math.abs(this.lastXVelo - this.body.velocity.x) * -1);
+                this.storedVelo += Math.max(Math.abs(this.lastXVelo - this.body.velocity.x) * -1, -87.5);
+            }
+            this.lastXVelo = this.body.velocity.x;
+
+            // cap jump velo
+            if (Math.abs(this.storedVelo) > this.maxStoredVelo) {
+                this.storedVelo = this.maxStoredVelo * -1;
+            }
+        }*/
+        this.handleControlInputs();
+
+        // cap downwards velo so you don't fall through the floor (thanks phaser)
+        this.body.velocity.y = Math.min(this.body.velocity.y, this.maxFallVelo);
+        console.log(this.storedVelo);
+    }
+
+    handleControlInputs() {
+        // left/right movement
+        if (this.body.onFloor()) {
+            // determine movement direction, set drag, and animate player model
+            if (this.controls.left.isDown && !this.controls.right.isDown) {
+                this.moveDirection = -1;
+                this.body.setDragX(0);
+                this.rotation += (Math.PI / -6 - this.rotation) * 0.01;
+            }
+            else if (this.controls.right.isDown && !this.controls.left.isDown) {
+                this.moveDirection = 1;
+                this.body.setDragX(0);
+                this.rotation += (Math.PI / 6 - this.rotation) * 0.01;
+            }
+            else {
+                if (this.controls.left.isDown) {
+                    this.body.setDragX(this.drag);
+                    this.rotation -= this.rotation * 0.01;
+                }
+                else {
+                    this.body.setDragX(this.drag * 3);
+                    this.rotation -= this.rotation * 0.03;
+                }
+                this.moveDirection = 0;
+            }
+
+            // update player physics values
+            this.body.setAccelerationX(this.acceleration * this.moveDirection);
+
+            // handle velocity storage
+            if (this.canStoreVelo && this.controls.jump.isDown) {
+                // kill acceleration and update drag if needed
+                this.body.setAccelerationX(0);
+                if (this.body.drag.x < this.drag) {
+                    this.body.setDragX(this.drag);
+                }
+
+                // add siphoned xVelo to storage
+                if (Math.abs(this.lastXVelo) < this.body.maxVelocity.x && Math.abs(this.storedVelo) < this.maxStoredVelo) {
+                    // if you hit a wall, you don't get to keep the velo from that
+                    this.storedVelo += Math.min(Math.abs(this.lastXVelo - this.body.velocity.x), 87.5);
+                }
+                this.lastXVelo = this.body.velocity.x;
+
+                // cap stored velo
+                this.storedVelo = Math.min(this.storedVelo, this.maxStoredVelo);
             }
         }
     }
