6da5299738bb420d490ccd3dd9b42512364b75ea
qk9 <iain6475@gmail.com>
2026-05-15 21:42:52 -0700

    finished player movement, added a few options for background speedometer rendering

--- src/main.js
+++ src/main.js
@@ -22,11 +22,12 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            debug: false,
             gravity: {
                 x: 0,
                 y: 0
-            }
+            },
+            fps: 120
         }
     },
     width: 1600,
